aa1d3c7dd2515fbaf73b4bf93d587bc7a41fca17
Italo Mauricio <105960758+italo-mauricio@users.noreply.github.com>
2022-10-03 19:40:02 -0300

    adicionando aula de condicionais

---
+++ Aula5/ex008.js
@@ -0,0 +1,13 @@
+var vel = 59
+
+console.log(`A velocidade do seu carro é ${vel}km/h `)
+
+if (vel > 60){
+
+    console.log("Você foi multado!")
+
+}
+
+console.log("Dirija sempre usando cinto!")
+
+
