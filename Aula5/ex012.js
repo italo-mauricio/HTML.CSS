aeec01f2640331a65d0c25d52009b9246e60d80d
italo <italogithub@gmail.com>
2022-11-02 19:48:06 -0300

    trabalhando com switch case

---
+++ Aula5/ex012.js
@@ -0,0 +1,42 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+console.log(diaSem)
+
+
+/*
+    Domingo = 0
+    Segunda = 1
+    Terça = 2
+    Quarta = 3
+    Quinta = 4
+    Sexta = 5
+    Sábado = 6
+*/
+
+switch (diaSem) {
+    case 0:
+        console.log('Domingo')
+        break;
+    case 1:
+        console.log('Segunda')
+        break;
+    case 2:
+        console.log('Terça')
+        break;
+    case 3:
+        console.log('Quarta')
+        break;
+    case 4:
+        console.log('Quinta')
+        break;
+    case 5:
+        console.log('Sexta')
+        break;
+    case 6:
+        console.log('Sábado')
+        break;
+
+    default:
+        console.log('Opção inválida!')
+        break;
+}
