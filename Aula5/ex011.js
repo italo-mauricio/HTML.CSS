5293568b7759cfbfc309c4e1969c189a924f41a7
italo <italogithub@gmail.com>
2022-11-02 18:39:24 -0300

    condicionais em JS

---
+++ Aula5/ex011.js
@@ -0,0 +1,10 @@
+var hora = 12
+console.log(`Agora são ${hora} horas. `)
+if (hora < 12){
+    console.log('Bom dia')
+}else if (hora <= 18){
+    console.log('Boa tarde')
+}else{
+    console.log('Boa noite')
+}
+ 
