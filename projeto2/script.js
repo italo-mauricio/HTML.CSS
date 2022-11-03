8c91bcc84d9018131f474f3d72baabbd5f3d0d93
italo <italogithub@gmail.com>
2022-11-03 16:50:02 -0300

    update

--- projeto2/script.js
+++ projeto2/script.js
@@ -2,7 +2,13 @@ function verificar(){
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
-     
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('Verifique os dados e tente novamente!')
+    }else{
+        window.alert('Tudo ok!')
+    }
+
 }
 
 
