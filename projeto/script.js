0768c31ed6bebdf8c82b6e5cb6e784674c822127
italo <italogithub@gmail.com>
2022-11-03 10:45:32 -0300

    update

--- projeto/script.js
+++ projeto/script.js
@@ -1,11 +1,23 @@
 function carregar(){
     var msg = window.document.getElementById('msg')
+    var msg2 = window.document.getElementById('msg2')
+    var msg3 = window.document.getElementById('msg3')
+    var msg4 = window.document.getElementById('msg4')
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    var dia = new Date()
+    var ndia = dia.getDate()
+    var mes = new Date()
+    var nmes = mes.getMonth()
+    var ano = new Date()
+    var nano = ano.getFullYear()
 
     
     msg.innerHTML = `Agora são ${hora} horas.` 
+    msg2.innerHTML = `Dia ${ndia}`
+    msg3.innerHTML = `Mês ${nmes}`
+    msg4.innerHTML = `Ano ${nano}`
     if (hora >= 0 && hora < 12){
         img.src = 'imagem-manha.png'
         document.body.style.background = '#e2cd9f'
@@ -21,5 +33,4 @@ function carregar(){
 }
 
 
-// 2cd9f
-// 
+
