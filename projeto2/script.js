050b805d6e5efa6d434669914ee4ff06be31ec06
italo <italogithub@gmail.com>
2022-11-03 17:36:50 -0300

    update

--- projeto2/script.js
+++ projeto2/script.js
@@ -5,8 +5,17 @@ function verificar(){
     var res = document.getElementById('res')
     if (fano.value.length == 0 || fano.value > ano) {
         window.alert('Verifique os dados e tente novamente!')
-    }else{
-        window.alert('Tudo ok!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        if (fsex[0].checked){
+            gênero = 'Homem'
+        } else if (fsex[1].checked){
+            gênero = 'Mulher'
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
     }
 
 }
